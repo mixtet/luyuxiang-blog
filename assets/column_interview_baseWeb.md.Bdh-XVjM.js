8f955545b13563b1738bf4e672a634d290f72fa3
mixtet <3195341483@qq.com>
2024-05-24 19:40:38 +0800

    deploy

---
+++ assets/column_interview_baseWeb.md.Bdh-XVjM.js
@@ -0,0 +1,34 @@
+import{_ as s,c as a,o as i,a1 as n}from"./chunks/framework.CPTkhf9l.js";const g=JSON.parse('{"title":"vscode 快捷键","description":"","frontmatter":{},"headers":[],"relativePath":"column/interview/baseWeb.md","filePath":"column/interview/baseWeb.md"}'),l={name:"column/interview/baseWeb.md"},p=n(`<h1 id="vscode-快捷键" tabindex="-1">vscode 快捷键 <a class="header-anchor" href="#vscode-快捷键" aria-label="Permalink to &quot;vscode 快捷键&quot;">​</a></h1><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code"><code><span class="line"><span>command + shift + p 快速调出setting等</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>command + D 快速选择相同内容</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>command + N 新建文件 （不好用，还得自己去保存路径）</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>option + shift + ↑/↓  向上/下复制一行</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>option + ↑/↓ 向上/下移动</span></span></code></pre></div><h1 id="html-标签" tabindex="-1">HTML 标签 <a class="header-anchor" href="#html-标签" aria-label="Permalink to &quot;HTML 标签&quot;">​</a></h1><h2 id="a" tabindex="-1">a <a class="header-anchor" href="#a" aria-label="Permalink to &quot;a&quot;">​</a></h2><div class="language-html vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">html</span><pre class="shiki shiki-themes github-light github-dark vp-code"><code><span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;">&lt;!-- 跳转到这个页面，id为top的位置 --&gt;</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">&lt;</span><span style="--shiki-light:#22863A;--shiki-dark:#85E89D;">a</span><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;"> href</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">=</span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">&quot;2.html#top&quot;</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">&gt;&lt;/</span><span style="--shiki-light:#22863A;--shiki-dark:#85E89D;">a</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">&gt;</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;">&lt;!-- 跳转到对应的电子邮箱 --&gt;</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">&lt;</span><span style="--shiki-light:#22863A;--shiki-dark:#85E89D;">a</span><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;"> href</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">=</span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">&quot;mailto:1342234@qq.com&quot;</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">&gt;&lt;/</span><span style="--shiki-light:#22863A;--shiki-dark:#85E89D;">a</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">&gt;</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;">&lt;!-- 去打电话，一般用于手机直接调起打电话页面 --&gt;</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">&lt;</span><span style="--shiki-light:#22863A;--shiki-dark:#85E89D;">a</span><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;"> href</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">=</span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">&quot;tel:15877051148&quot;</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">&gt;&lt;/</span><span style="--shiki-light:#22863A;--shiki-dark:#85E89D;">a</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">&gt;</span></span></code></pre></div><h2 id="css" tabindex="-1">css <a class="header-anchor" href="#css" aria-label="Permalink to &quot;css&quot;">​</a></h2><div class="language-css vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">css</span><pre class="shiki shiki-themes github-light github-dark vp-code"><code><span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">// 会选中某元素中（必须是块级元素）第一行的第一个字母</span></span>
+<span class="line"><span style="--shiki-light:#22863A;--shiki-dark:#85E89D;">p</span><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">::first-letter</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> {</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">}</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">// 会选中某元素中 （必须是块级元素）第一行 全部文字</span></span>
+<span class="line"><span style="--shiki-light:#22863A;--shiki-dark:#85E89D;">p</span><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">::first-line</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> {</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">}</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">// 页面上鼠标选择的字体颜色</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">::selection</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> {</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">}</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">// 段落缩进两个字符</span></span>
+<span class="line"><span style="--shiki-light:#22863A;--shiki-dark:#85E89D;">text-indet</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">: 2em;</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">// 相对于自己进行定位，不脱离文档流，自己的位置还在</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">position: relative;</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">top: 100px;</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">left: 20px;</span></span></code></pre></div>`,7),t=[p];function e(h,k,E,d,c,r){return i(),a("div",null,t)}const y=s(l,[["render",e]]);export{g as __pageData,y as default};
