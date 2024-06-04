c152821cf6528383beb36f4cf786f08a685d89be
mixtet <3195341483@qq.com>
2024-06-04 17:29:55 +0800

    deploy

---
+++ assets/column_abount_index.md.EvMKKi5z.js
@@ -0,0 +1,62 @@
+import{_ as l,o as t,c as a,j as n,F as d,E as p,a as o,t as c,p as _,l as m,I as b}from"./chunks/framework.CLEf5sEq.js";const u=e=>(_("data-v-ae70abb3"),e=e(),m(),e),v={class:"person"},S={class:"schedule"},P={class:"item"},h=u(()=>n("br",null,null,-1)),g=u(()=>n("pre",null,`            教育背景：
+            2016.09～2020.06 广西民族大学相思湖学院  计算机科学与技术(本科)
+            在校获奖情况： 2016年荣获校运会三人板鞋第八名；2017年荣获院级举办“颂歌十五年，感恩前行”读书主题活动之征文大赛优秀奖；2017年荣获计工系体运动会跳长绳第二名；2016年荣获院运会先进个人
+            专业课程:　　web前端网页设计、移动应用界面设计、软件工程、算法分析与设计、数据结构、iOS、Linux、Java、PHP、Android应用开发、Photoshop、数据库系统、C语言程序设计、Swift程序设计等等
+            
+            技能：
+            熟悉HTML，CSS，JavaScript，TypeScript，Sass，Less，HTML5/CSS3新特性等的前端基础技能。
+            熟练掌握Vue2/Vue3及周边生态，Element-ui，Ant Design Vue等的框架开发，熟悉微信小程序开发，熟悉uni-app开发。
+            熟悉Node.js。
+            熟悉Git。
+            了解Fultter, React等前端框架。
+            了解Webpack、Vite、Babel等前端构建工具。
+            了解WebSocket。
+
+            工作经验：
+            广州营客信息科技有限公司 初级前端工程师 2022.03～2023.12
+            任职期间负责小程序，pc后台的内容模块及设计师版的维护及更新迭代。负责自定义页面日常维护及使用vue3重构自定义页面。
+
+            时间印记（广州）网络有限公司 前端工程师 2021-03～2022-02
+            负责优化职场通页面，使客户更加容易上手。
+            负责广告系统后台开发，使用Ant Design 框架开发系统后台，通过合理的组件化、模块化以及代码组织，减少开发时间。使用vue-draggable-resizable-gorkys对广告系统窗口进行定位及改变广告窗口的大小，使用ECharts展示广告系统分布统计数据，使得数据更加直观。
+            负责南京物业管理系统PC端开发，使用高德地图API进行地图选点，使客户更快找到意向的位置，使用异步搜索，根据输入的字段模糊搜索出对应的楼盘数据，设计了页面的兼容与响应式方案，通过运用服务端渲染、静态资源缓存等技术，使得页面加载更快。
+            负责财媒通的后台、PC端、小程序开发。后台使用若依框架进行二开，开发时间低于预估时间30%。PC端设计了页面的兼容性与响应式方案，使得页面不会因为窗口的大小而混乱。客服系统对接腾讯IM进行客服与客户之间的消息通信。小程序使用微信源生小程序开发。
+            负责开发剧本杀，使用WebSocket对游戏流程进行控制，使得游戏按照流程往下走，不会导致游戏流程乱套，即使游戏中途退出又进、网络中断等原因导致游戏中断，也不会影响到其它玩家的游戏流程的进行，使用Vuex对游戏进程进行状态管理以及缓存游戏基本信息。
+            配合开发三件事，融易租(支付宝小程序)，深圳卫健委问卷后台管理系统，奔图小程序（微信）。
+
+            广西金兰湾云软件有限公司 web前端工程师 2020-04～2021-01
+            与后端配合进行数据交互，实现页面数据动态加载；
+            基于vue脚手架、element-ui、mint-ui及微信公众号开发前端页面；
+            模块化开发，提高代码复用性，健壮性，可维护性；
+            负责酒店系统移动端的开发。
+
+            项目经验
+
+            广告后台管理系统 
+            项目描述：提供给广告商家管理ipad屏幕上的广告展示位置，及窗口大小
+            项目职责：搭建后台前端基础框架，根据原型图完成页面制作，配合后端联调接口。
+            应用技术：使用Vue.js + Ant Design开发后台管理系统，使用vue-draggable-resizable-gorkys对广告窗口大小及初始化位置。使用Router进行页面的权限管理。
+            项目总结：这个项目是组长给我提供思路去做，从零开始把后台系统的整个流程从头开始开发了一遍，也让我更加了解后台的菜单权限管理、Router、Vuex的使用，从中受益良多，为之后开发后台系统及使用其它框架或者集成框架打下基础，看别人代码的不再毫无方向，看不懂的状态。
+
+            南京物业系统
+            项目职责：配合后端开发后台，后台使用PHP；负责开发PC端网页。
+            应用技术：1. 后台使用PHP开发。
+                    2. 使用腾讯地图API进行楼盘定位。
+                    3.使用JavaScript、Jquery对上传的楼盘，楼层图片进行楼房、楼层的标记。
+            地址：www.yaliheng.com
+
+            财媒通
+            项目职责：基于若依框架进行二开，开发财媒通的后台管理系统；使用Vue.js + Element-ui开发，财媒通PC端网页；使用微信源生小程序开发小程序端。
+            应用技术：1. 集成第三方流程框架，开发自定义工作流程，使用Vuex缓存数据。
+                    2.使用腾讯IM API开发客服系统。
+                    3. 使用Swipei实现PC端的轮播图播放及商家的卡片式展示。
+                    4. 使用element-ui中的Layout布局实现网页响应式开发。
+                    5.使用CSS3中的transform实现页面的卡片式的动画翻转。
+            
+            酒店系统 
+            项目描述：此项目主要用来给酒店客人，订房订单下单的功能，本项目移动端开发使用vue+微信公众号等，PC端使用vue+element-ui等开发管理后台。
+            项目职责：根据需求完成页面制作，显示，与后端配合动态渲染数据；根据项目经理修改页面bug及优化项目页面。
+            应用技术：页面使用flex布局，使用vue+mint-ui/vant/element-ui开发单页面应用，使用axios连接后台数据；使用html2canvas生成海报。 
+
+
+        `,-1)),x={__name:"index",setup(e){const s=[{id:1e4,timeStart:"2022.03",timeEnd:"2023.12",title:"广州营客信息科技有限公司",content:"任职期间负责小程序，pc后台的内容模块及设计师版的维护及更新迭代。负责自定义页面日常维护及使用vue3重构自定义页面。",job:"初级前端工程师"},{id:2e4,timeStart:"2021-03",timeEnd:"2022-02",title:"时间印记（广州）网络有限公司",content:"任职期间负责小程序，pc后台的内容模块及设计师版的维护及更新迭代。负责自定义页面日常维护及使用vue3重构自定义页面。",job:"前端工程师"},{id:3e4,timeStart:"2020-04",timeEnd:"2021-01",title:"广西金兰湾云软件有限公司",content:"任职期间负责小程序，pc后台的内容模块及设计师版的维护及更新迭代。负责自定义页面日常维护及使用vue3重构自定义页面。",job:"web前端工程师"}];return(r,f)=>(t(),a("div",v,[n("div",S,[(t(),a(d,null,p(s,i=>n("div",P,[n("div",null,[o(c(i.title)+" ",1),h,o(" "+c(i.content),1)])])),64))]),g]))}},C=l(x,[["__scopeId","data-v-ae70abb3"]]),w=JSON.parse('{"title":"","description":"","frontmatter":{"pageClass":"site-layout"},"headers":[],"relativePath":"column/abount/index.md","filePath":"column/abount/index.md"}'),V={name:"column/abount/index.md"},y=Object.assign(V,{setup(e){return(s,r)=>(t(),a("div",null,[b(C)]))}});export{w as __pageData,y as default};
